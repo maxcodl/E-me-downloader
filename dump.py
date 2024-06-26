2372cb85af87e69fe3f80d6d0674282f47e09cd6
Max <125237755+maxcodl@users.noreply.github.com>
2024-06-26 12:31:57 +0530

    Update dump.py

--- dump.py
+++ dump.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 
 session = requests.Session()
 
-
 def collect_links(album_url):
     parsed_url = urlparse(album_url)
     if parsed_url.hostname != "www.erome.com":
@@ -22,15 +21,12 @@ def collect_links(album_url):
     soup = BeautifulSoup(r.content, "html.parser")
     title = soup.find("meta", property="og:title")["content"]
     videos = [video_source["src"] for video_source in soup.find_all("source")]
-    images = [
-        image["data-src"] for image in soup.find_all("img", {"class": "img-back"})
-    ]
+    images = [image["data-src"] for image in soup.find_all("img", {"class": "img-back"})]
     urls = list(set(videos + images))
     download_path = get_final_path(title)
     existing_files = get_files_in_dir(download_path)
     for file_url in urls:
         download(file_url, download_path, album_url, existing_files)
-
 
 def get_final_path(title):
     final_path = os.path.join("downloads", title)
@@ -38,14 +34,10 @@ def get_final_path(title):
         os.makedirs(final_path)
     return final_path
 
-
 def get_files_in_dir(directory):
     if not os.path.exists(directory):
         return []
-    return [
-        f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))
-    ]
-
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
 def download(url, download_path, album=None, existing_files=[]):
     parsed_url = urlparse(url)
@@ -69,7 +61,7 @@ def download(url, download_path, album=None, existing_files=[]):
             r.raise_for_status()
             total_size = int(r.headers.get('content-length', 0))
             with open(os.path.join(download_path, file_name), "wb") as f, tqdm(
-                total=total_size, unit='B', unit_scale=True, desc=file_name, ncols=100
+                total=total_size, unit='B', unit_scale=True, desc=file_name, ncols=100, file=sys.stdout, leave=True
             ) as pbar:
                 for chunk in r.iter_content(chunk_size=1024):
                     if chunk:
@@ -77,7 +69,6 @@ def download(url, download_path, album=None, existing_files=[]):
                         pbar.update(len(chunk))
     except requests.RequestException as e:
         print(f'[ERROR] Download of "{url}" failed: {e}')
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download media from erome.com albums.")
